e40ff2c5eb0033d0150a368d38ce7e32ac351448
lminjung99 <82351454+lminjung99@users.noreply.github.com>
2022-08-17 09:54:22 +0900

    [Bronze V] Title: 긴자리 계산, Time: 112 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.kt
@@ -0,0 +1,10 @@
+import java.math.BigInteger
+
+fun main(){
+    var num1 = BigInteger(readLine()!!)
+    var num2 = BigInteger(readLine()!!)
+    println(num1.add(num2))
+    println(num1.subtract((num2)))
+    println(num1.multiply(num2))
+    
+}
